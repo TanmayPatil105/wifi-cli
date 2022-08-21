b5923942d772f263eacede717afbce344a81c665
Tanmay Patil <92677342+TanmayPatil105@users.noreply.github.com>
2022-08-21 14:01:56 +0530

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-chmod +x ./src/wifi
-sudo cp ./src/wifi /bin/
-sudo mkdir /usr/share/wifi
-sudo cp ./src/help.txt /usr/share/wifi/
+if [ $# == 0 ]
+then
+	chmod +x ./src/wifi
+	sudo cp ./src/wifi /bin/
+	if [ -d "/usr/share/wifi/" ]
+	then
+		echo "Wifi already exists"
+	else
+		sudo mkdir /usr/share/wifi
+	fi	
+	sudo cp ./src/help.txt /usr/share/wifi/
+	echo "Installed Succesfully"
+	exit 1
+fi
+if [ $1 == "remove" ]
+then
+	sudo rm /bin/wifi 
+	sudo rm /usr/share/wifi/help.txt
+	sudo rmdir /usr/share/wifi/
+	exit 1
+fi
