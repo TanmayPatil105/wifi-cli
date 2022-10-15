03f51a93fc39c9e334b5c631959886a2c848a430
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-10-15 18:30:05 +0530

    modified installer.sh

--- installer.sh
+++ installer.sh
@@ -9,30 +9,31 @@ then
 	if [ -f "$EXE" ]
 	then
 		sudo rm /bin/wifi
+		echo "Removing $EXE.."
 	fi
-	sudo cp ./src/wifi /bin/
+
 	if [ -d "$DIR" ]
 	then
 		sudo rm -rf "$DIR"
-		sudo mkdir -p "$DIR"
 		echo "Removing $DIR.."
-		echo "Removing $EXE.."
-
-	else
-		sudo mkdir -p "$DIR"
 	fi	
-	if  [ -f "$DIR"/help.txt ]
-	then
-		sudo rm "$DIR"/help.txt
-	fi
+
+	echo "Creating $EXE..."
+	sudo cp ./src/wifi /bin/
+	sudo mkdir -p "$DIR"
 	sudo cp ./src/help.txt "$DIR"/
 	echo "Installed Succesfully"
 	exit 1
 fi
+
 if [ $1 == "remove" ]
 then
+	timeout 5s echo "Uninstalling wifi.."
+	echo "Removing $EXE"
 	sudo rm /bin/wifi 
+	echo "Removing $DIR/help.txt..."
 	sudo rm /usr/share/wifi/help.txt
+	echo "Removed $DIR.."
 	sudo rmdir /usr/share/wifi/
 	echo "Uninstalled Succesfully"
 	exit 1
