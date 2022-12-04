5fc350b2001b94f1a4b02565b62619362eb1646f
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-12-04 18:46:11 +0530

    Removed help.txt

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-DIR="/usr/share/wifi"
 EXE="/bin/wifi"
 
 if [ $# == 0 ]
@@ -11,19 +10,9 @@ then
 		echo "Removing $EXE.."
 	fi
 
-	if [ -d "$DIR" ]
-	then
-		sudo rm -rf "$DIR"
-		echo "Removing $DIR.."
-	fi	
-
 	echo "Creating $EXE..."
 	sudo cp ./src/wifi /bin/
 
-	echo "Creating $DIR..."
-	sudo mkdir -p "$DIR"
-
-	sudo cp ./src/help.txt "$DIR"/
 	echo "Installed Succesfully"
 fi
 
@@ -36,12 +25,6 @@ then
 
 		echo "Removing $EXE"
 		sudo rm /bin/wifi 
-
-		echo "Removing $DIR/help.txt..."
-		sudo rm /usr/share/wifi/help.txt
-
-		echo "Removed $DIR.."
-		sudo rmdir /usr/share/wifi/
 
 		echo "Uninstalled Succesfully"
 	fi
